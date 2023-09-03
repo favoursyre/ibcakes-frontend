f7ba5e5b984b2e3dd6a652447513a4e850f22a5a
favoursyre <favourforchrist91@gmail.com>
2023-09-03 14:20:42 +0100

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default async function RootLayout({
   const contacts = await getContacts()
 
   return (
-    <html lang="en" className={`${styles.html}`}>
+    <html lang="en">
       <body>
         <Header />
         {/* <ToastContainer /> */}
